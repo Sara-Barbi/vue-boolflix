66a5905d687923bc0a0f3a85891d88a70a1a4b83
Sara-Barbi <barbisara02@gmail.com>
2022-02-03 10:32:34 +0100

    initialization

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    
+    <Headers />
+    <Mains />
+    <Footers />
+  </div>
+</template>
+
+<script>
+import Heladers from './components/Headers.vue'
+import Mains from './components/Mains.vue'
+import Footers from './components/Footers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Headers,
+    Mains,
+    Footers
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
