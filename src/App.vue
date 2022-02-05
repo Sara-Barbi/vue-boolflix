dae812de689ae2d01947fc826fd58fb7194a1f62
Sara-Barbi <barbisara02@gmail.com>
2022-02-05 19:41:57 +0100

    scritta relativa a stelle

--- src/App.vue
+++ src/App.vue
@@ -117,10 +117,7 @@ export default {
       this.input= inputFiltro;
 
       },
-      getStelle(num){
-         Math.round((num)/2).toFixed(0)
-      }
-
+     
     }
   
 }
--- src/components/sub-components/filmFiltrati.vue
+++ src/components/sub-components/filmFiltrati.vue
@@ -4,7 +4,14 @@
     <li>{{prodotto.titolo}}</li>
     <li>{{prodotto.titolo_originale}}</li>     
     <img :src="require(`../../assets/img/${prodotto.lingua}.png`)" alt="">
-    <li>{{prodotto.voto}}</li>
+
+    <li v-if="getStelle(prodotto.voto)==0">5 stelle vuote</li>
+    <li v-else-if="getStelle(prodotto.voto)==1">4stelle vuote,1 piena</li>
+    <li v-else-if="getStelle(prodotto.voto)==2">3stelle vuote, 2 piene</li>
+    <li v-else-if="getStelle(prodotto.voto)==3">2 stelle vuote, 3 piene</li>
+    <li v-else-if="getStelle(prodotto.voto)==4">1 stella vuota,4 piene</li>
+    <li v-else>0 stella vuota,5 piene</li>
+
    
 </div>
   
@@ -15,6 +22,12 @@ export default {
   name:'filmFiltrati',
   props:{                          
     prodotto: Object
+
+  },
+  methods:{
+    getStelle(num){
+        return Math.round((num)/2).toFixed(0)
+    }
 
   }
 }
