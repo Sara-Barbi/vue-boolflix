d6e9b9a08ee9c4b3f51db46c28266c0aacc26d83
Sara-Barbi <barbisara02@gmail.com>
2022-02-06 15:51:55 +0100

    stelle colorate

--- src/components/sub-components/filmFiltrati.vue
+++ src/components/sub-components/filmFiltrati.vue
@@ -5,12 +5,12 @@
     <li>{{prodotto.titolo_originale}}</li>     
     <img :src="require(`../../assets/img/${prodotto.lingua}.png`)" alt="">
 
-    <li v-if="getStelle(prodotto.voto)==0">5 stelle vuote</li>
-    <li v-else-if="getStelle(prodotto.voto)==1">4stelle vuote,1 piena</li>
-    <li v-else-if="getStelle(prodotto.voto)==2">3stelle vuote, 2 piene</li>
-    <li v-else-if="getStelle(prodotto.voto)==3">2 stelle vuote, 3 piene</li>
-    <li v-else-if="getStelle(prodotto.voto)==4">1 stella vuota,4 piene</li>
-    <li v-else>0 stella vuota,5 piene</li>
+    <li v-if="getStelle(prodotto.voto)==0"><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i></li>
+    <li v-else-if="getStelle(prodotto.voto)==1"><i class="bi bi-star-fill"></i><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i></li>
+    <li v-else-if="getStelle(prodotto.voto)==2"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star"></i><i class="bi bi-star"></i><i class="bi bi-star"></i></li>
+    <li v-else-if="getStelle(prodotto.voto)==3"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star"></i><i class="bi bi-star"></i></li>
+    <li v-else-if="getStelle(prodotto.voto)==4"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star"></i></li>
+    <li v-else><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></li>
 
    
 </div>
@@ -34,6 +34,10 @@ export default {
 </script>
 
 <style>
-        
+  @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"); 
+  .bi-star-fill{
+    color: rgb(233, 233, 91);
+  }
+  
 
 </style>
